20d788472cfee66fc89876d74ae7b1aa065b2936
wangyubo <1061322005@qq.com>
2019-01-04 17:00:05 +0800

    添加全连接神经网络代码，未完成

--- 3.FC/fullConnectNN.py
+++ 3.FC/fullConnectNN.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import random
 
 
 # 节点类，负责记录和维护节点自身信息以及与这个节点相关的上下游连接，实现输出值和误差项的计算
@@ -56,3 +57,171 @@ class Node(object):
         '''
         节点属于输出层，计算delta
         '''
+        self.delta = self.output * (1 - self.output) * (label - self.output)
+
+    def __str__(self):
+        '''
+        打印节点信息
+        '''
+        node_str = '%u-%u: output: %f delta: %f' % (self.layer_index, self.node_index, self.output, self.delta)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        upstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.upstream, '')
+        return node_str + '\n\tdownstream:' + downstream_str + '\n\tupstream:' + upstream_str
+
+
+class ConstNode(object):
+    def __init__(self, layer_index, node_index):
+        '''
+        构造节点对象。
+        :param layer_index: 节点对象所属的层的编号
+        :param node_index: 节点的编号
+        '''
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.downstream = []
+        self.output = 1
+
+    def append_downstream_connection(self, conn):
+        '''
+        添加一个到下游节点的连接
+        '''
+        self.downstream.append(conn)
+
+    def calc_hidden_layer_delta(self):
+        '''
+        节点属于隐藏层时，计算delta
+        '''
+        downstream_delta = reduce(
+            lambda ret, conn: ret + conn.downstream_node.delta * conn.weight,
+            self.downstream,
+            0.0
+        )
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def calc_output_layer_delta(self, label):
+        '''
+        节点属于输出层，计算delta
+        '''
+        self.delta = self.output * (1 - self.output) * (label - self.output)
+
+    def __str__(self):
+        '''
+        打印节点信息
+        '''
+        node_str = '%u-%u: output: 1' % (self.layer_index, self.node_index)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        return node_str + '\n\tdownstream:' + downstream_str
+
+
+class Layer(object):
+    def __init__(self, layer_index, node_count):
+        '''
+        初始化一层
+        :param layer_index: 层编号
+        :param node_count: 层所包含的节点个数
+        '''
+        self.layer_index = layer_index
+        self.nodes = []
+        for i in range(len(node_count)):
+            self.nodes.append(Node(layer_index, i))
+        self.nodes.append(ConstNode(layer_index, node_count))
+
+    def set_output(self, data):
+        '''
+        设置层的输出，层是输入层时会用到
+        '''
+        for i in range(len(data)):
+            self.nodes[i].set_output(data[i])
+
+    def calc_output(self):
+        '''
+        计算层的输出向量
+        '''
+        for node in self.nodes[:-1]:
+            node.calc_output()
+
+    def dump(self):
+        '''
+        打印层的信息
+        '''
+        for node in self.nodes:
+            print(node)
+
+
+class Connection(object):
+    def __init__(self, upstream_node, downstream_node):
+        '''
+        初始化连接，权重初始化为是一个很小的随机数
+        :param upstream_node: 连接的上游节点
+        :param downstream_node: 连接的下有节点
+        '''
+        self.upstream_node = upstream_node
+        self.downstream_node = downstream_node
+        self.weight = random.uniform(-0.1, 0.1)
+        self.gradient = 0.0
+
+    def calc_gradient(self):
+        '''
+        计算梯度
+        '''
+        self.gradient = self.downstream_node.delta * self.upstream_node.output
+
+    def get_gradient(self):
+        '''
+        获取当前的梯度
+        '''
+        return self.gradient
+
+    def update_weight(self, rate):
+        '''
+        根据梯度下降算法更新权重
+        '''
+        self.calc_gradient()
+        self.weight += rate * self.gradient
+
+    def __str__(self):
+        '''
+        打印连接信息
+        '''
+        return '(%u-%u) -> (%u-%u) = %f' % (
+            self.upstream_node.layer_index,
+            self.upstream_node.node_index,
+            self.downstream_node.layer_index,
+            self.downstream_node.node_index,
+            self.weight
+        )
+
+
+class Connections(object):
+    def __init__(self):
+        self.connections = []
+
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+    def dump(self):
+        for conn in self.connections:
+            print(conn)
+
+class Network(object):
+    def __init__(self, layers):
+        '''
+        初始化一个全连接神经网络
+        :param layers: 二维数组，描述神经网络每层节点数
+        '''
+        self.connections = Connections()
+        self.layers = []
+        layer_count = len(layers)
+        node_count = 0
+        for i in range(layer_count):
+            self.layers.append(Layer(i, layers[i]))
+        for layer in range(layer_count - 1):
+            connections = [Connection(upstream_node, downstream_node)
+                           for upstream_node in self.layers[layer].nodes
+                           for downstream_node in self.layers[layer + 1].nodes[:-1]]
+            for conn in connections:
+                self.connections.add_connection(conn)
+                conn.downstream_node.append_upstream_connection(conn)
+                conn.upstream_node.append_downstream_connection(conn)
+                
+
